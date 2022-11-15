c8b59a45af5ddac940ed8705dcc477185c688db0
Gabriel Hernández <gabrielymarg@gmail.com>
2022-11-15 16:31:51 -0300

    Added cluster / fork mode...

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 // Load dotenv
 require('dotenv').config()
 
+// Cluster
+const cluster = require('cluster')
+
+// CPUs
+const processor_count = require('os').cpus().length
+
 // Yargs
 const yargs = require('yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
 const port = argv.port || 8000
-
-// Express
-const express = require('express')
-const server = express()
+const mode = argv.mode || 'fork'
 
 // Mongo
 const {Types} = require('mongoose')
@@ -64,14 +67,44 @@ const hbs = handlebars.create({
     layoutsDir: __dirname + '/views/layouts/'
 })
 
-// Server config and middlewares
-server.engine('hbs', hbs.engine)
-server.set('view engine', 'hbs')
-server.set('views', 'views')
-server.use(express.json())
-server.use(express.urlencoded({extended: true}))
-server.use(express.static('public'))
-server.use(session({
+// Router
+const ramdomRouter =  require('./routes/randomRouter').randomRouter
+
+// Server Cluster / Fork
+if (cluster.isMaster) {
+    console.log(`Cantidad de núcleos disponibles: ${processor_count}`)
+    console.log(`Hilo principal en el proceso PID: ${process.pid}`)
+    // Cluster
+    if (mode === 'cluster') {
+        for (let i = 0; i < processor_count; i++) {
+            cluster.fork()
+        }
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`Worker ${worker.process.pid} terminó.`)
+            console.log('Iniciando otro worker...')
+            cluster.fork()
+        })
+    } else {
+        cluster.fork()
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`Worker ${worker.process.pid} terminó.`)
+            console.log('Iniciando otro worker...')
+            cluster.fork()
+        })
+    }
+} else {
+    // Express
+    const express = require('express')
+    const server = express()
+
+    // Server config and middlewares
+    server.engine('hbs', hbs.engine)
+    server.set('view engine', 'hbs')
+    server.set('views', 'views')
+    server.use(express.json())
+    server.use(express.urlencoded({extended: true}))
+    server.use(express.static('public'))
+    server.use(session({
     secret: 'top-secret',
     resave: false,
     saveUninitialized: true,
@@ -80,71 +113,70 @@ server.use(session({
         retries: 0,
         ttl: 60 * 10 // 10 minutes session
     })
-}))
-server.use(passport.initialize())
-server.use(passport.session())
+    }))
+    server.use(passport.initialize())
+    server.use(passport.session())
+    server.use('/api/random', ramdomRouter)
 
-// Router
-const ramdomRouter =  require('./routes/randomRouter').randomRouter
-server.use('/api/random', ramdomRouter)
-
-// Routes
-server.get('/', getAuth, (req, res) => {
-    res.render('index', {username: req.user.username})
-})
-
-server.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/public/signin.html')
-})
-
-server.get('/logout', (req, res) => {
-    res.render('logout')
-})
-
-server.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/public/signup.html')
-})
-
-server.get('/info', getAuth, (req, res) => {
-    res.render('info', {
-        argvs: process.argv.slice(2),
-        platform: process.platform,
-        node: process.version,
-        memory: process.memoryUsage().rss,
-        path: process.execPath,
-        pid: process.pid,
-        folder: process.cwd()
+    // Routes
+    server.get('/', getAuth, (req, res) => {
+        res.render('index', {username: req.user.username})
     })
-})
 
-server.post('/signin', passport.authenticate('signin', {
-    failureRedirect: '/login?error=signin' // SignIn error view implemented on login screen
-}), (req, res) => {
-    req.session.user = req.user
+    server.get('/login', (req, res) => {
+        res.sendFile(__dirname + '/public/signin.html')
+    })
+
+    server.get('/logout', (req, res) => {
+        res.render('logout')
+    })
+
+    server.get('/register', (req, res) => {
+        res.sendFile(__dirname + '/public/signup.html')
+    })
+
+    server.get('/info', getAuth, (req, res) => {
+        res.render('info', {
+            argvs: process.argv.slice(2),
+            platform: process.platform,
+            node: process.version,
+            memory: process.memoryUsage().rss,
+            path: process.execPath,
+            pid: process.pid,
+            folder: process.cwd(),
+            cpus: processor_count
+        })
+    })
+
+    server.post('/signin', passport.authenticate('signin', {
+        failureRedirect: '/login?error=signin' // SignIn error view implemented on login screen
+    }), (req, res) => {
+        req.session.user = req.user
     res.redirect('/')
-})
+    })
 
-server.post('/signup', passport.authenticate('signup', {
-    failureRedirect: '/login?error=exist' // SignUp error view implemented on login screen
-}), (req, res) => {
-    req.session.user = req.user
+    server.post('/signup', passport.authenticate('signup', {
+        failureRedirect: '/login?error=exist' // SignUp error view implemented on login screen
+    }), (req, res) => {
+        req.session.user = req.user
     res.redirect('/')
-})
+    })
 
-server.post('/signout', (req, res) => {
-    const username = req.user.fullname
+    server.post('/signout', (req, res) => {
+        const username = req.user.fullname
     req.session.destroy()
     req.logout(() => {
         res.redirect(`/logout?username=${username}`)
     })
-})
+    })
 
-// Server start
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+    // Server start
+    server.listen(port, () => {
+        console.log(`Server running on port ${port}`)
+    })
 
-// Server error
-server.on('error', (err) => {
-    console.error(`Server error: ${err.message}`)
-})
+    // Server error
+    server.on('error', (err) => {
+        console.error(`Server error: ${err.message}`)
+    })
+}
